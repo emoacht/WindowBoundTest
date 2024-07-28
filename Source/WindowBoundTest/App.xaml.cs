48b3e378954a690ecc55579972bd42d65a2df4a9
emoacht <emotom@pobox.com>
2024-07-28 16:31:03 +0900

    Add EnableMultiMonitorDisplayClipping as comment

--- Source/WindowBoundTest/App.xaml.cs
+++ Source/WindowBoundTest/App.xaml.cs
@@ -13,5 +13,12 @@ namespace WindowBoundTest
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			// https://learn.microsoft.com/en-us/dotnet/framework/migration-guide/mitigation-wpf-window-rendering
+			//CoreCompatibilityPreferences.EnableMultiMonitorDisplayClipping = true;
+
+			base.OnStartup(e);
+		}
 	}
 }
